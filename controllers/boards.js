5887e7f9298c16b32ba24269d6b2ed64d9ce302b
Babenko Liudmyla <savelieva0509@gmail.com>
2024-05-12 20:07:50 +0300

    update boards api

--- controllers/boards.js
+++ controllers/boards.js
@@ -2,47 +2,49 @@ const { Board } = require("../models/board");
 
 const { HttpError, ControllerWrapper } = require("../helpers");
 
-const listDashboards = async (req, res) => {
+const listBoards = async (req, res) => {
   const result = await Board.find();
   res.json(result);
 };
 
-// const getDashboardById = async (req, res) => {
-//   const { dashboardId } = req.params;
-//   const result = await dashboards.getDashboardById(dashboardId);
-//   if (!result) {
-//     throw HttpError(404, "Not found");
-//   }
-//   res.json(result);
-// };
+const getBoardById = async (req, res) => {
+  const { boardId } = req.params;
+  const result = await Board.findById(boardId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
 
-const addDashboard = async (req, res) => {
+const addBoard = async (req, res) => {
   const result = await Board.create(req.body);
   res.status(201).json(result);
 };
 
-// const updateDashboardById = async (req, res) => {
-//   const { dashboardId } = req.params;
-//   const result = await dashboards.updateDashboardById(dashboardId, req.body);
-//   if (!result) {
-//     throw HttpError(404, "Not found");
-//   }
-//   res.json(result);
-// };
+const updateBoardById = async (req, res) => {
+  const { boardId } = req.params;
+  const result = await Board.findByIdAndUpdate(boardId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
 
-// const removeDashboard = async (req, res, next) => {
-//   const { dashboardId } = req.params;
-//   const result = await dashboards.removeDashboard(dashboardId);
-//   if (!result) {
-//     throw HttpError(404, "Not found");
-//   }
-//   res.json({ message: "Delete success" });
-// };
+const removeBoard = async (req, res, next) => {
+  const { boardId } = req.params;
+  const result = await Board.findOneAndDelete(boardId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({ message: "Delete success" });
+};
 
 module.exports = {
-  listDashboards: ControllerWrapper(listDashboards),
-  // getDashboardById: ControllerWrapper(getDashboardById),
-  addDashboard: ControllerWrapper(addDashboard),
-  // updateDashboardById: ControllerWrapper(updateDashboardById),
-  // removeDashboard: ControllerWrapper(removeDashboard),
+  listBoards: ControllerWrapper(listBoards),
+  getBoardById: ControllerWrapper(getBoardById),
+  addBoard: ControllerWrapper(addBoard),
+  updateBoardById: ControllerWrapper(updateBoardById),
+  removeBoard: ControllerWrapper(removeBoard),
 };
---
+++ middlewares/isValidId.js
@@ -0,0 +1,13 @@
+const { isValidObjectId } = require("mongoose");
+
+const { HttpError } = require("../helpers");
+
+const isValidId = (req, res, next) => {
+  const { boardId } = req.params;
+  if (!isValidObjectId(boardId)) {
+    next(HttpError(400, `${boardId} is not valid id`));
+  }
+  next();
+};
+
+module.exports = isValidId;
