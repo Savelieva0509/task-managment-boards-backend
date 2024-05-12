4502419d1bf83bda7f07ee581d3f9cdec662f262
Babenko Liudmyla <savelieva0509@gmail.com>
2024-05-12 18:30:31 +0300

    Create board.js

---
+++ models/board.js
@@ -0,0 +1,25 @@
+const Joi = require("joi");
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../helpers");
+
+const boardSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+boardSchema.post("save", handleMongooseError);
+
+const addSchema = Joi.object({
+  title: Joi.string().required(),
+});
+
+const schemas = {
+  addSchema,
+};
+const Board = model("board", boardSchema);
+module.exports = { Board, schemas };
