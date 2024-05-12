baec21799f747c1cf9d8b7d9347f677017215efd
Babenko Liudmyla <savelieva0509@gmail.com>
2024-05-12 13:14:00 +0300

    add process.env

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://savelieva0509:W5SEVhhJ3axtppS9@cluster0.rhhmecl.mongodb.net/boards_reader?retryWrites=true&w=majority&appName=Cluster0";
+const { DB_HOST, PORT = 3001 } = process.env;
 
-//const { DB_HOST, PORT = 3001 } = process.env;
-// mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3001, () => {
+    app.listen(PORT, () => {
       console.log("Database connection successful");
     });
   })
